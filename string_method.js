43bcfcd62a146933c9af8ef7a7f5032447e51899
Srujan17181 <srujanatsruju1718@gmail.com>
2026-02-27 19:03:41 +0530

    string method
    1.slice
    2.concatination
    3.uppercase 
    4.lowercase

---
+++ string_method.js
@@ -0,0 +1,8 @@
+// var message=prompt("enter the message");
+// var len=message.length
+
+// console.log("you enter "+ " "+ len + " "+ "of charecters" +" "+ "remaining charecter you can write"+" "+ (180-len))
+
+var tweet= prompt("tweet what u wish ");
+var tweetcount=tweet.slice(0,50)
+alert("out of region")
